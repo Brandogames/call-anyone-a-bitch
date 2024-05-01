ef5da50ab0b7f07dc9520222856fb0aa7f3dadd1
Brandogames <97616011+Brandogames@users.noreply.github.com>
2024-05-01 10:52:41 -0400

    hi

--- bored_programming.js
+++ bored_programming.js
@@ -1,9 +1,6 @@
 function bored() {
-document.getElementById("hi").style="display:blocked";
-document.getElementById("bored").style="display:none";
-}
-function bored2() {
-window.close()
+document.getElementById("hi").style="display:blocked"
+document.getElementById("bored").style="display:none"
 }
 const audio = new Audio();
 audio.src = "./bitch.mp3";
