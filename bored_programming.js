19d27ecdda65db505657b0aa248d18d0f47dc034
Brandogames <97616011+Brandogames@users.noreply.github.com>
2024-05-01 10:47:09 -0400

    Update bored_programming.js

--- bored_programming.js
+++ bored_programming.js
@@ -1,10 +1,9 @@
 function bored() {
-document.getElementById("hi").style="display:blocked"
-document.getElementById("bored").style="display:none"
+document.getElementById("hi").style="display:blocked";
+document.getElementById("bored").style="display:none";
 }
 function bored2() {
 window.close()
 }
-
 const audio = new Audio();
 audio.src = "./bitch.mp3";
